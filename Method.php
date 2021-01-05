bbacdd437d1e77a590e729e5c80ec7e8c8578f90
kelompok-10pbo <76929094+kelompok-10pbo@users.noreply.github.com>
2021-01-05 05:13:10 +0000

    Create Method.php

---
+++ Method.php
@@ -0,0 +1,19 @@
+<?php
+//cara penulisan class dan property OOP PHP
+class motor{
+    //property OOP
+    var $hitam;
+    var $beat;
+    var $180;
+
+    //method OOP
+    function maju(){
+    //motor saya sedang maju
+}
+    function berhenti(){
+    //motor saya sedang berhenti
+    }
+
+}
+
+?>
