514321d985c115741686fee25c562f77c6465363
kelompok-10pbo <76929094+kelompok-10pbo@users.noreply.github.com>
2021-01-22 03:34:22 +0000

    Create Skrip3.php

---
+++ Skrip3.php
@@ -0,0 +1,12 @@
+<?php
+interface motor{
+    Public function sen_kanan();
+    Public function sen_kiri();
+}
+class kendaraan implements motor{
+      //... isi dari class kendaraan 
+}
+class motor matic implements motor{
+     //... isi dari class motor matic
+}
+?>
